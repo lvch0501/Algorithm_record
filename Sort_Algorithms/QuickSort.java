a662bccf02a087c7482868d6554673839869e9e6
920986596@qq.com <920986596@qq.com>
2019-12-17 17:25:52 +0800

    quicksort

--- Sort_Algorithms/QuickSort.java
+++ Sort_Algorithms/QuickSort.java
@@ -1,13 +1,25 @@
 import java.util.Arrays;
 
-public class PartitionSort {
+public class QuickSort {
 
     public static void main(String[] args) {
         int[] a = {2,3,4,5,1,9,2,7,4};
-        paritition(a,0, a.length-1);
+        quickSort(a, 0, a.length-1);
         System.out.println(Arrays.toString(a));
     }
-    public static void paritition(int[] arr, int left, int right){
+
+    public static void quickSort(int[] arr, int left, int right){
+        if(left<right){
+            swap(arr, left+(int)(Math.random()*(right-left)), right);
+
+            int[] par = partition(arr, left, right);
+            System.out.println(Arrays.toString(par));
+            quickSort(arr, left, par[0]-1);
+            quickSort(arr, par[1]+1, right);
+        }
+    }
+
+    public static int[] partition(int[] arr, int left, int right){
         int less = left-1;
         int last = right;
         while(left<right){
@@ -20,6 +32,10 @@ public class PartitionSort {
             }
         }
         swap(arr, last, right);
+        int[] res = new int[2];
+        res[0] = less+1;
+        res[1] = right;
+        return res;
     }
 
     public static void swap(int[] arr, int a, int b){
