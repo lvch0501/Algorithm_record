ed42f8b207e0058f3e3b7db07357165204d489c7
920986596@qq.com <920986596@qq.com>
2019-12-18 21:51:56 +0800

    init

--- Data_Structure/StackMin.java
+++ Data_Structure/StackMin.java
@@ -10,7 +10,36 @@ public class StackMin {
     }
 
     public void push(int num){
+        if(this.stack2.isEmpty()){
+            stack2.push(num);
+        }else if(this.stack2.peek()>=num){
+            stack2.push(num);
+        }else {
+            stack2.push(stack2.peek());
+        }
+        stack1.push(num);
+    }
 
+    public Integer peek(){
+        if(stack1.isEmpty()){
+            return null;
+        }
+        return stack1.peek();
+    }
+
+    public Integer getMin(){
+        if(stack2.isEmpty()){
+            return null;
+        }
+        return stack2.peek();
+    }
+
+    public Integer pop(){
+        if(stack1.isEmpty()){
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        stack2.pop();
+        return stack1.pop();
     }
 
 }
