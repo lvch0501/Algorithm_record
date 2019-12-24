3407c892570f620b2ddc5c36e659ef02ff3ad323
920986596@qq.com <920986596@qq.com>
2019-12-24 15:46:42 +0800

    print matrix

--- Data_Structure/PrintMatrixSpiralOrder.java
+++ Data_Structure/PrintMatrixSpiralOrder.java
@@ -1,16 +1,48 @@
 public class PrintMatrixSpiralOrder {
+
+
     // 顺时针打印矩阵中的所有元素
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1}};
+        spoiralOrderPrint(matrix);
+    }
     public static void spoiralOrderPrint(int[][] matrix){
         int row1 = 0;
         int col1 = 0;
         int row2 = matrix.length-1;
         int col2 = matrix[0].length-1;
-
+        while (row1<=row2&&col1<=col2){
+            printEdge(matrix, row1++, col1++, row2--, col2--);
+        }
 
     }
 
-    public static void printEdge(int[][] matrix, int row1, int col1, int row){
-
+    public static void printEdge(int[][] matrix, int row1, int col1, int row2, int col2){
+        if(row1==row2){
+            while(col1<=col2){
+                System.out.print(matrix[row1][col1++]+" ");
+            }
+        }else if(col1==col2){
+            while (row1<=row2){
+                System.out.print(matrix[row1++][col1]+" ");
+            }
+        }else {
+            int newrow1 = row1;
+            int newcol1 = col1;
+            while(col1<col2){
+                System.out.print(matrix[row1][col1++]+" ");
+            }
+            while(row1<row2){
+                System.out.print(matrix[row1++][col2]+" ");
+            }
+            while(col2>newcol1){
+                System.out.print(matrix[row2][col2--]+" ");
+            }
+            while(row2>newrow1) {
+                System.out.print(matrix[row2--][newcol1]+" ");
+            }
+        }
     }
 
 }
--- Sort_Algorithms/Test.java
+++ Sort_Algorithms/Test.java
@@ -1,57 +1,61 @@
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Test {
     public static void main(String[] args) {
-        int[] a = {3, 5, 1, 2, 6, 3, 11, 5};
-        HeapSort(a);
-        System.out.println(Arrays.toString(a));
-    }
-
-
-
-    public static void HeapSort(int[] a){
-        if(a.length==0||a.length==1){
-            return;
-        }
-        for(int i=1; i<a.length; i++){
-            CreateMaxHeap(a, i);
-        }
-
-        int heapSize = a.length-1;
-        swap(a, 0, heapSize--);
-
-        while(heapSize>0){
-            adjustHeap(a, heapSize);
-            swap(a, 0, heapSize--);
-        }
-
-
-    }
-
-    public static void CreateMaxHeap(int[] arr, int start){
-        while(start>0){
-            if(arr[start]>arr[(start-1)/2]){
-                swap(arr, start, (start-1)/2);
+        Student[] sArr = new Student[3];
+        sArr[0] = new Student(15, 170, "lv");
+        sArr[1] = new Student(10, 180, "chen");
+        sArr[2] = new Student(10, 190, "hui");
+        System.out.println(Arrays.toString(sArr));
+        Arrays.sort(sArr, new Comparator<Student>() {
+            @Override
+            public int compare(Student o1, Student o2) {
+                return o1.getAge()-o2.getAge();
             }
-            start = (start-1)/2;
-        }
+        });
+        System.out.println(Arrays.toString(sArr));
+    }
+}
+
+class Student{
+    private int age;
+    private int height;
+
+    public int getAge() {
+        return age;
     }
 
-    public static void adjustHeap(int[] arr, int size){
-        int start = 1;
-        while(start<size){
-            int index = start+1<=size&&arr[start+1]>arr[start]?start+1:start;
-            int largest = arr[index]>arr[(start-1)/2]?arr[index]:arr[(start-1)/2];
-            if(largest==arr[(start-1)/2]){
-                return;
-            }
-            swap(arr, (start-1)/2, index);
-            start = index*2+1;
-        }
+    public void setAge(int age) {
+        this.age = age;
     }
-    public static void swap(int[] arr, int a, int b){
-        int temp = arr[a];
-        arr[a] = arr[b];
-        arr[b] = temp;
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+    Student(int age, int height, String name){
+        this.age = age;
+        this.height = height;
+        this.name = name;
+    }
+
+
+    public String toString(){
+        return super.toString()+"[age: "+this.age+", height"+this.height+", name:"+this.name+"]";
     }
 }
