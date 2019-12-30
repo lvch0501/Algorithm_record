362c4b98fa90a47a623ea28d9d05b6bbf0ce1884
920986596@qq.com <920986596@qq.com>
2019-12-30 15:27:04 +0800

    randomPool

---
+++ Data_Structure/RandomPool.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+
+public class RandomPool {
+    int size;
+    HashMap<String, Integer> map1;
+    HashMap<Integer, String> map2;
+
+    RandomPool(){
+        map1 = new HashMap<>();
+        map2 = new HashMap<>();
+        size = 0;
+    }
+
+    public void insert(String str){
+        map1.put(str, ++size);
+        map2.put(size, str);
+    }
+
+    public void delete(String str){
+        int index = map1.get(str);
+        map1.remove(str);
+        map2.remove(index);
+        if(size!=index){
+            map2.put(index, map2.get(size));
+            map2.remove(size);
+            map1.put(map2.get(size), index);
+        }
+
+    }
+
+    public String getRandom(){
+        return map2.get((int)(Math.random()*size));
+
+    }
+
+}
