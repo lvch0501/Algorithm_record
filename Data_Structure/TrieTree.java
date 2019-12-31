8554832561df3cc35d111b4c50209989039a5fa4
920986596@qq.com <920986596@qq.com>
2019-12-31 15:38:23 +0800

    TrieTree

---
+++ Data_Structure/TrieTree.java
@@ -0,0 +1,78 @@
+import javax.swing.tree.TreeNode;
+
+public class TrieTree {
+
+    public static class TrieNode{
+        public int path;
+        public int end;
+        public TrieNode[] nexts;
+
+        TrieNode(){
+            path = 0;
+            end = 0;
+            nexts = new TrieNode[26];
+        }
+    }
+
+
+    public static class Trie{
+        private TrieNode root;
+
+        Trie(){
+            root = new TrieNode();
+        }
+
+
+        public void  insert(String word){
+            if (word==null)return;
+
+            char[] chs = word.toCharArray();
+            int index = 0;
+            TrieNode node = root;
+            for(int i=0; i<chs.length; i++){
+                index = chs[0]-'a';
+                if(node.nexts[index]==null){
+                    node.nexts[index] = new TrieNode();
+                }
+                node = node.nexts[index];
+                node.path++;
+            }
+            node.end++;
+
+        }
+
+
+        public int search(String word){
+            if(word==null){
+                return 0;
+            }
+
+            char[] chs = word.toCharArray();
+
+            TrieNode node = root;
+            int index;
+            for(int i=0; i<chs.length; i++){
+                index = chs[i] - 'a';
+                if(node.nexts[index]==null){
+                    return 0;
+                }
+                node = node.nexts[index];
+            }
+            return node.end;
+
+        }
+
+        public void delete(String word){
+            if(search(word)!=0){
+                char[] chs = word.toCharArray();
+                TrieNode node = root;
+                int index;
+            }
+        }
+
+        public int prefixNum(String pre){
+
+        }
+    }
+
+}
