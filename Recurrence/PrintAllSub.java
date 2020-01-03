25d2c86a88b12e200a122d26935d68668fc13bb9
920986596@qq.com <920986596@qq.com>
2020-01-03 15:39:49 +0800

    print all sub

--- Recurrence/PrintAllSub.java
+++ Recurrence/PrintAllSub.java
@@ -4,4 +4,13 @@ public class PrintAllSub {
     }
 
 
+
+    public static void printSub(char[] chs, int n, String s){
+        if(n==chs.length){
+            System.out.println(s);
+            return;
+        }
+        printSub(chs, n+1, s);
+        printSub(chs, n+1, s+String.valueOf(chs[n]));
+    }
 }
